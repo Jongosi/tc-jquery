4e8ebc2b887e74bbce5d568b08dfde2ee05a5576
Jason <jason@twincreations.co.uk>
2013-07-11 23:13:14 +0100

    Changed the method to check if jQuery has already been included on page.

--- js/tc_jquery/tc-jquery-loader.js
+++ js/tc_jquery/tc-jquery-loader.js
@@ -4,6 +4,6 @@
  */
 
 // check for jQuery and load if necessary
-if ( (typeof jQuery === 'undefined') || !window.jQuery ) {
+if ( (typeof jQuery === 'undefined') || (window.jQuery === undefined) ) {
     document.write(unescape("%3Cscript type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js'%3E%3C/script%3E"));
 }
